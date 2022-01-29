e2ff81a449228b74ec0172328f8dbb43deeaeae5
Simon <76248532+SimonGreenwood@users.noreply.github.com>
2022-01-29 21:00:22 +0000

    Built

--- src/App.js
+++ src/App.js
@@ -3,21 +3,73 @@ import SubmitName from "./components/SubmitName"
 import axios from "axios"
 import Data from "./components/Data"
 const App = () => {
+  const [newName, setNewName] = useState("")
+  const [submitted,setSubmitted] = useState(false)
+  const [ageLoaded,setAgeLoaded] = useState(false)
+  const [genderLoaded, setGenderLoaded] = useState(false)
+  const [nationLoaded, setNationLoaded] = useState(false)
+  const [age,setAge] = useState("")
+  const [gender,setGender] = useState("")
+  const [nation,setNation] = useState("")
   const handleSubmit = (event) => {
     console.log(`Submitted ${newName}`)
+    setSubmitted(true)
     event.preventDefault()
-    setShowData(newName)
+    console.log(newName)
+    setAgeLoaded(false)
+    setGenderLoaded(false)
+    setNationLoaded(false)
+    getAgeData(newName)
+    getGenderData(newName)
+    getNationData(newName)
     setNewName("")
   }
-  const [newName, setNewName] = useState("")
-  const [showData,setShowData] = useState(null)
-  if(showData) {
-    return (
-      <div>
-        <SubmitName name={newName} onNameChange={(event) => setNewName(event.target.value)} onSubmit={event => handleSubmit(event)}/>
-        <Data showData={showData}/>
-      </div>
-    ) 
+  const getAgeData = (name) => {
+    return axios
+      .get("https://api.agify.io?name="+name)
+      .then(response => {
+        setAge(response.data)
+        setAgeLoaded(true)
+        console.log(age)
+      })
+  }
+  const getGenderData = (name) => {
+    return axios
+    .get("https://api.genderize.io?name="+name)
+    .then(response => {
+      setGender(response.data)
+      setGenderLoaded(true)
+      console.log(gender)
+    })
+  }
+  const getNationData = (name) => {
+    return axios
+      .get("https://api.nationalize.io?name="+name)
+      .then(response => {
+        setNation(response.data)
+        setNationLoaded(true)
+        console.log(nation)
+      })
+  }
+
+  if(submitted){
+
+    if(ageLoaded && genderLoaded && nationLoaded) {
+      console.log(age,gender,nation)
+      return (
+        <div>
+          <SubmitName name={newName} onNameChange={(event) => setNewName(event.target.value)} onSubmit={event => handleSubmit(event)}/>
+          <Data ageData={age} genderData={gender} nationData={nation}/>
+        </div>
+      ) 
+    } else {
+      return (
+        <div>
+          <SubmitName name={newName} onNameChange={(event) => setNewName(event.target.value)} onSubmit={event => handleSubmit(event)}/>
+          <h1>Loading Data...</h1>
+        </div>
+      ) 
+    }
   } else {
     return (
       <div>
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,29 +1,13 @@
 
 import axios from "axios";
 import React, {useEffect, useState} from "react";
-const Data = ({showData}) => {
-  const [ageData, setAgeData] = useState("")
-  const [genderData, setGenderData] = useState("")
-  const [nationData, setNationData] = useState("")
-  const [loaded,setLoaded] = useState(false)
+const Data = ({showData,ageData,genderData,nationData}) => {
+  // const [ageData, setAgeData] = useState("")
+  // const [genderData, setGenderData] = useState("")
+  // const [nationData, setNationData] = useState("")
   const regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
-  const hook = () => {
-    console.log("Loading Data")
-    axios
-      .get("https://api.agify.io?name="+showData)
-      .then(response => setAgeData(response.data))
-    axios
-      .get("https://api.genderize.io?name="+showData)
-      .then(response => setGenderData(response.data))
-    axios
-      .get("https://api.nationalize.io?name="+showData)
-      .then(response => {
-        setNationData(response.data)
-        setLoaded(true)
-      })
-  }
-     useEffect(hook,[])
-  if (loaded) {
+  console.log(ageData,nationData,genderData)
+  if (ageData.count!=0&&genderData.count!=0) {
     return (
       <div>
         <h1>Age Predictor</h1>
@@ -32,12 +16,14 @@ const Data = ({showData}) => {
         <h2>We are {genderData.probability*100}% sure that {genderData.name} is {genderData.gender}</h2>
         <h1>Nation Predictor</h1>
         <h2>We think that {nationData.name} is from:</h2>
-        <ul>{nationData.country.map(country => <li key={country.country_id} >{regionNames.of(country.country_id)} {Math.round(country.probability*10000)/100}%</li>)}</ul>
+        <ul>
+          {nationData.country.map(country => <li key={country.country_id} >{regionNames.of(country.country_id)} {Math.round(country.probability*10000)/100}%</li>)}
+        </ul>
       </div>
     )
   } else {
     return (
-      <h1>Loading...</h1>
+      <h1>Invalid name!</h1>
     )
   }
 }
