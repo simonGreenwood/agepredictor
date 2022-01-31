bd9b19c571927514293ac00298ce4b595f40be1a
Simon <76248532+SimonGreenwood@users.noreply.github.com>
2022-01-31 10:28:31 +0000

    Updated site link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div>
-            <p>Created by Simon. Source code available at <a href="http://www.mypage.com" target="_blank" rel="noopener">https://github.com/simonGreenwood/agepredictor</a></p>
+            <p>Created by Simon. Source code available at <a href="https://github.com/simonGreenwood/agepredictor" target="_blank" rel="noopener">https://github.com/simonGreenwood/agepredictor</a></p>
         </div>
     )
 }
